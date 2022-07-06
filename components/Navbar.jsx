3deee08a2253312fadf56b9c2f1471ce7019fdeb
harjot <harjot23062000@gmail.com>
2022-07-06 15:55:11 -0700

    navbar done for lucid read

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,10 +4,7 @@ import React, { useEffect, useState } from "react";
 import {
   FaBars,
   FaTimes,
-  FaFacebook,
-  FaGithub,
-  FaTwitter,
-  FaLinkedin,
+ 
 } from "react-icons/fa";
 
 const Navbar = () => {
@@ -45,109 +42,32 @@ const Navbar = () => {
     },
   ];
 
-  return (
-    <div
-      className={`w-full h-20 z-10 fixed bg-white text-black duration-300 ease-in ${
-        pageScroll && "bg-black text-[#fff]"
-      }`}
-    >
-      <div className="flex justify-between items-center w-full h-full max-w-screen-xl mx-auto p-4">
-        <Link href="/#home">
-          <h1 className="text-3xl lg:text-4xl font-bold uppercase underline underline-offset-2 tracking-wider cursor-pointer">
-            harjot
-          </h1>
-        </Link>
+  return (<><div className="fixed h-16 w-full duration-300 ease-in  bg-slate-100">
+    <div className="flex justify-between items-center w-full h-full mx-auto max-w-screen-xl ">
+       <h1>Logo</h1> 
+       <div>
 
-        <div>
-          <ul className="hidden md:flex">
-            {links.map(({ id, link }) => (
-              <Link key={id} href={`/#${link}`}>
-                <li className="ml-10 text-sm uppercase cursor-pointer duration-200 ease-out hover:scale-105 tracking-wider">
-                  {link}
-                </li>
-              </Link>
-            ))}
-          </ul>
+        <h1>Search</h1>
+       </div>
+       <div>
 
-          {!navigation && (
-            <div
-              className="md:hidden cursor-pointer"
-              onClick={() => setNavigation(true)}
-            >
-              <FaBars size={30} />
-            </div>
-          )}
-        </div>
-      </div>
+        <h1>lucid read</h1>
+       </div>
 
-      <div
-        className={
-          navigation
-            ? "md:hidden fixed left-0 top-0 w-full h-full bg-black/70 backdrop-blur"
-            : ""
-        }
-      >
-        <div
-          className={
-            navigation
-              ? "fixed left-0 top-0 w-4/5 h-full bg-gradient-to-r from-blue-600 to-cyan-600 text-white p-10 ease-in duration-500"
-              : "fixed top-0 left-[-100%] p-10 h-full ease-in duration-500"
-          }
-        >
-          <div>
-            <div className="flex w-full items-center justify-between">
-              <Link href="/#home">
-                <h2
-                  onClick={() => setNavigation(false)}
-                  className="text-3xl font-bold uppercase underline underline-offset-2 tracking-wider cursor-pointer"
-                >
-                  harjot
-                </h2>
-              </Link>
-              <div
-                onClick={() => setNavigation(false)}
-                className="p-3 cursor-pointer"
-              >
-                <FaTimes size={30} />
-              </div>
-            </div>
-          </div>
+       
+       
+       
+       
+       </div>
+    
+    
 
-          <div className="mt-24 flex flex-col h-fit gap-20">
-            <ul className="uppercase">
-              {links.map(({ id, link }) => (
-                <Link key={id} href={`/#${link}`}>
-                  <li
-                    onClick={() => setNavigation(false)}
-                    className="py-4 text-2xl tracking-wider cursor-pointer"
-                  >
-                    {link}
-                  </li>
-                </Link>
-              ))}
-            </ul>
+     
 
-            <div>
-              <div className="grid grid-cols-2 mx-auto w-4/5 gap-10">
-                <div className="flex items-center justify-center rounded-full shadow-md shadow-white p-3 cursor-pointer">
-                  <FaLinkedin size={25} />
-                </div>
-                <div className="flex items-center justify-center rounded-full shadow-md shadow-white p-3 cursor-pointer">
-                  <FaTwitter size={25} />
-                </div>
-                <div className="flex items-center justify-center rounded-full shadow-md shadow-white p-3 cursor-pointer">
-                  <FaFacebook size={25} />
-                </div>
-                <div className="flex items-center justify-center rounded-full shadow-md shadow-white p-3 cursor-pointer">
-                  <FaGithub size={25} />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+
+
+  </div>
+  </>
   );
-};
-
+  };
 export default Navbar;
